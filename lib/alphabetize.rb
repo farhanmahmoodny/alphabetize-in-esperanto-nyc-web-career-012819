61c416973665c6f4585565e1d1482de1542d4d55
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-21 01:12:16 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,7 @@ def alphabetize(arr)
   arr.sort! do |a|
     new_array = []
     a.each_char do |char|
-      new_array.push(ESPERANTO_ALPHABET.index(char))
+      new_array << ESPERANTO_ALPHABET.index(char)
     end
     new_array
   end
